7f3669fa06fef4e3cdfc79ac8d058367fd8e25dd
Sandeep kumar <sandeepk@geekyants.com>
2023-03-06 12:53:02 +0530

    chore: add storybook docs

---
+++ storybook/.storybook/preview.js
@@ -0,0 +1,21 @@
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  docs: {
+    // inlineStories: false,
+  },
+  options: {
+    storySort: {
+      method: 'alphabetical',
+      order: [
+        'Graphql SDL to JSON',
+        ['Getting Started', 'Requirements', 'How to Install'],
+      ]
+    },
+  },
+};
